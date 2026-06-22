5fe0f63eb767b22c8f7971eab1549931cb3228f8
joonho2910-hue <joonho2910@gmail.com>
2026-06-22 17:19:38 +0900

    6.22

---
+++ python/a08_operator.py
@@ -0,0 +1,27 @@
+class Add_test:
+    def __add__(self, other):
+        return "더하기 연산이 수행되었다."
+
+def main():
+    print(2 ** 4)
+    print(2 ** 64)
+    print(18/4)
+    print(type(18/4))
+    
+    print(18//4)
+    print(type(18//4))
+    
+    print(14 % 4)
+    a = Add_test()
+    b = Add_test()
+    print(a + b)
+    print(a + 123)
+    print(a + 3.14)
+    print("abcd" * 5)
+    a = 5
+    a += 1
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
